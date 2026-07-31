dd3ef9a33a5af565af808a855d4c2373c1c5f421
celineleedle <celineleedle@gmail.com>
2026-07-31 16:34:33 -0400

    put registers to separate file from cpu

---
+++ gb/internal/cpu/registers.go
@@ -0,0 +1,48 @@
+package cpu
+
+type Registers struct {
+	A, F uint8 // accumulator & flags
+	B, C uint8
+	D, E uint8
+	H, L uint8
+	SP   uint16 // stack pointer
+	PC   uint16 // program counter
+}
+
+func (r *Registers) AF() uint16 { return uint16(r.A)<<8 | uint16(r.F) }
+func (r *Registers) BC() uint16 { return uint16(r.B)<<8 | uint16(r.C) }
+func (r *Registers) DE() uint16 { return uint16(r.D)<<8 | uint16(r.E) }
+func (r *Registers) HL() uint16 { return uint16(r.H)<<8 | uint16(r.L) }
+
+const zeroBitIndex = 7
+const subtractionBitIndex = 6
+const halfCarryBitIndex = 5
+const carryBitIndex = 4
+
+func (r *Registers) SetFlag(bitIndex int, value bool) {
+	if value {
+		r.F |= uint8(1) << bitIndex
+	} else {
+		r.F &= ^(uint8(1) << bitIndex)
+	}
+}
+
+func (r *Registers) SetAF(value uint16) {
+	r.A = uint8(value >> 8)
+	r.F = uint8(value & 0xF0) // flag register uses bits 7, 6, 5, 4 only
+}
+
+func (r *Registers) SetBC(value uint16) {
+	r.B = uint8(value >> 8)
+	r.C = uint8(value & 0xFF)
+}
+
+func (r *Registers) SetDE(value uint16) {
+	r.D = uint8(value >> 8)
+	r.E = uint8(value & 0xFF)
+}
+
+func (r *Registers) SetHL(value uint16) {
+	r.H = uint8(value >> 8)
+	r.L = uint8(value & 0xFF)
+}
