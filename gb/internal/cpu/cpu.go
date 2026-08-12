be5a93f4e60e7f364040dd3793b2c9acd9e94c84
celineleedle <celineleedle@gmail.com>
2026-08-12 16:38:26 -0400

    add read and write cycle functions, time advance commented out initially

--- gb/internal/cpu/cpu.go
+++ gb/internal/cpu/cpu.go
@@ -12,16 +12,33 @@ type Cpu struct {
 	bus *bus.Bus
 }
 
+func (c *Cpu) ReadCycle(address uint16) (byte, error) {
+	// advance time
+	// c.bus.Tick(4) // 4 t-cycles / 1 m-cycle for a read
+
+	return c.bus.ReadByteAt(address)
+}
+
+func (c *Cpu) WriteCycle(address uint16, value byte) error {
+	// advance time
+	// c.bus.Tick(4) // 4 t-cycles / 1 m-cycle for a write
+
+	return c.bus.WriteByteAt(address, value)
+}
+
 func (c *Cpu) Step() int {
-	opcode, err := c.bus.ReadByteAt(c.registers.PC)
+	// fetch opcode at PC
+	opcode, err := c.ReadCycle(c.registers.PC)
 	if err != nil {
 		log.Fatalf("Error reading opcode at PC: 0x%04X: %v", c.registers.PC, err)
 	}
+	c.registers.PC++ // increment PC to point to next instruction
 
-	pc, cycles, err := c.handleOpcode(opcode)
+	pc, cycles, err := c.execute(opcode)
 	if err != nil {
 		log.Fatalf("Error executing opcode: 0x%02X at PC: 0x%04X: %v", opcode, c.registers.PC, err)
 	}
+	// TODO: add a debug boolean flag and gate these logs behind it.
 	log.Printf("Cycles executed: %d", cycles)
 	log.Printf("PC increment: %d", pc)
 
